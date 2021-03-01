189cc8500457f83393383525d409900880b43ce0
kingultron99 <lochym10@gmail.com>
2021-03-01 22:03:02 +1100

    now has working image modals and support for DMs

--- README.md
+++ README.md
@@ -3,7 +3,14 @@ Lets you view server icons and avatars in fullscreen via the context menu, or co
 
 [![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=Kings-Discord-Stuff&repo=avatar-icon-viewer&bg_color=1b1b1b&title_color=13a89e&text_color=e0e0e0&hide_border=true)](https://github.com/Kings-Discord-Stuff/avatar-icon-viewer)
 
+### Features!
+
+ - Grab/preview server icons
+ - Grab/preview user avatars
+ - Grab/preview group DM icons
+ - Grab/preview user DM avatars
+
 
 ### TODO
- - Image modals
- - DM & group DM context menus
+ - [x] ~~Image modals~~
+ - [x] ~~DM & group DM context menus~~
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { Plugin } from "@vizality/entities";
 import { patch, unpatch } from "@vizality/patcher";
 import { getModule, React } from "@vizality/webpack";
+const { open: openModal } = require('@vizality/modal');
+import { ImageModal, ContextMenu } from '@vizality/components';
 
 const copyToClipboard = require("electron").clipboard.writeText;
 
@@ -8,61 +10,77 @@ export default class AvatarIconViewer extends Plugin {
   start() {
     this._patchGuildMenu();
     this._patchUserMenu();
+    this._patchGroupDMMenu();
+    this._patchDMMenu();
   }
 
   _patchGuildMenu() {
-      const GuildContextMenu = getModule(
-        (m) => m.default && m.default.displayName === 'GuildContextMenu'
-      );
+      const GuildContextMenu = getModule(m => m.default && m.default.displayName === 'GuildContextMenu');
 
-      patch("guild-context", GuildContextMenu, "default", (props, res) => {
-        const { guild } = props[0];
-        const { icon }  = guild;
-        const url = `https://cdn.discordapp.com/icons/${guild.id}/${icon}.png?size=2048`;
+      patch("guild-context", GuildContextMenu, "default", ([{guild}], res) => {
           
-        res.props.children.splice(
-          0,
-          0,
-          this.createContext(url, "Icon")
-          );
+        res.props.children.splice(0, 0, this.createContext(guild.getIconURL(getModule(['hasAnimatedGuildIcon'], false).hasAnimatedAvatar(guild) ? "gif" : "webp").split("?")[0]+"?size=2048", "Icon"));
             
         return res;
       });
   }
 
   _patchUserMenu() {
-    const UserContextMenu = getModule(m => m.default && m.default.displayName && (m.default.displayName.endsWith("UserContextMenu") || m.default.displayName == "DMUserContextMenu"));
-
-    patch("user-context", UserContextMenu, "default", (props, res) => {
+    const UserContextMenu = getModule(m => m.default && m.default.displayName && (m.default.displayName.endsWith("UserContextMenu")));
+    
+    patch("user-context", UserContextMenu, "default", ([{user}], res) => { 
       
-      const { user } = props[0];
-      const { avatar } = user;
-
-      const url = `https://cdn.discordapp.com/avatars/${user.id}/${avatar}.png?size=2048`;
-
-
-      res.props.children.props.children.push(this.createContext(url, `Avatar`));
+      res.props.children.props.children.splice(0, 0, this.createContext(user.getAvatarURL(getModule(['hasAnimatedAvatar'], false).hasAnimatedAvatar(user) ? "gif" : "webp").split("?")[0]+"?size=2048",`Avatar`));
       return res;
     });
   }
+  
+  _patchGroupDMMenu() {
+    const GroupDMContextMenu = getModule(m => m.default && m.default.displayName && m.default.displayName.endsWith("GroupDMContextMenu"));
+    
+    patch('group-dm-context', GroupDMContextMenu, "default", ([{channel}], res) => {
+    
+      res.props.children.splice(0, 0, this.createContext(getModule('getChannelIconURL').getChannelIconURL(channel), `Group DM Icon`))
+
+      return res
+
+    });
+  }
+  _patchDMMenu() {
+    const DMUserContextMenu = getModule(m => m.default && m.default.displayName === "DMUserContextMenu");
+
+    patch("user-dm-menu", DMUserContextMenu, "default", ([{user}], res) => {
+      
+      res.props.children.props.children.splice(0, 0, this.createContext(user.getAvatarURL(getModule(['hasAnimatedAvatar'], false).hasAnimatedAvatar(user) ? "gif" : "webp").split("?")[0]+"?size=2048", "Avatar"))
+
+      return res
+    })
+
+  }
 
   createContext(url, type) {
-    const ContextMenu = getModule('MenuItem');
+    
+    return <ContextMenu.Group>
+      <ContextMenu.Item
+        id="aiw-view"
+        label={'Preview ' + type}
+        action={ () => openModal(() => ( <ImageModal src={url} width='2048' height='2048' /> )) }
+        />
 
-    return React.createElement(ContextMenu.MenuGroup, {
-      children: [
-        React.createElement(ContextMenu.MenuItem, {
-          label: "Copy " + type + " Link",
-          id: "aiv-copy",
-          action: () => copyToClipboard(url),
-        }),
-      ],
-    });
+        <ContextMenu.Item
+        id="aiw-copy"
+        label={'Copy ' + type + ' URL'}
+        action={ () => copyToClipboard(url) }
+        />
+    </ContextMenu.Group>
+
   }
 
   stop() {
     unpatch('guild-context');
     unpatch('server-menu');
-    unpatch("user-context");
+    unpatch('user-context');
+    unpatch('group-dm-context');
+    unpatch('user-dm-menu');
   }
 }
